d7bab9e97668dd47293407216b91157dffdeffbd
pnyekrasov <pnekrasov1910@gmail.com>
2023-08-30 18:02:56 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+// axios.defaults.baseURL = 'https://api.themoviedb.org/3';
+
+// export const fetchPopularMovies = async () => {
+//   const response = await axios('/trending/get-trending', options);
+//   console.log(response.data.results);
+//   return response.data.results;
+// };
 
 const options = {
   method: 'GET',
+  url: 'https://api.themoviedb.org/3/trending/movie/day',
+  params: { language: 'en-US' },
   headers: {
     accept: 'application/json',
     Authorization: 'Bearer 2cc1cdd3dc7a1b39cf1b283648efca77',
@@ -11,9 +19,17 @@ const options = {
 };
 
 export const fetchPopularMovies = async () => {
-  const response = await axios('/trending/get-trending', options);
-  console.log(response.data.results);
-  return response.data.results;
+  const response = await axios.request(options).then(function (response) {
+    console.log(response.data);
+  });
+  return response.data;
+
+  // .then(function (response) {
+  //   console.log(response.data);
+  // })
+  // .catch(function (error) {
+  //   console.error(error);
+  // });
 };
 
 // fetch('https://api.themoviedb.org/3//trending/get-trending', options)
